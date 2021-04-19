158fd9130e246c99b51f93a4a4e58b3419bf4ca6
86188 <997239885@qq.com>
2021-04-19 23:57:16 +0800

    test

---
+++ dockerfile-demo/src/main/java/com/example/demo/DockerfileDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerfileDemoApplication.class, args);
+	}
+	
+	@RequestMapping("/docker")	
+	public String getDocker() {
+		return "欢迎进入docker";
+	}
+
+}
