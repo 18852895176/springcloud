e6620f5688e8c691f6b17c05d8b6c40c08fa1910
huanggaokui <76521013+18852895176@users.noreply.github.com>
2021-04-21 21:20:28 +0800

    Update DockerfileDemoApplication.java

--- src/main/java/com/example/demo/DockerfileDemoApplication.java
+++ src/main/java/com/example/demo/DockerfileDemoApplication.java
@@ -15,7 +15,7 @@ public class DockerfileDemoApplication {
 	
 	@RequestMapping("/docker") 	
 	public String getDocker() {
-		return "欢迎进入docker----==342sdfsd"; 
+		return "欢迎进入docker---哦个人"; 
 	}
 
 }
